c44e71f3797b7acc77ad15f81ca6378992c0eb07
KateBahur <ebogurina@gmail.com>
2023-01-16 21:53:50 +0300

    fix some issues

--- Lesson_3/bytes.py
+++ Lesson_3/bytes.py
@@ -3,7 +3,7 @@ print(b1, type(b1))
 
 print('Байты'.encode('utf-8'))          # создание байтов
 
-a = bytes('bytes', encoding = 'utf-8')            # создание байтов
+a = bytes('bytes', encoding='utf-8')            # создание байтов
 print(a)
 
 b = bytes([50, 100, 125, 140])                         # создание байтов
@@ -14,7 +14,3 @@ print(x1)
 
 x3 = b'\xd0\x91\xd0\xb0\xd0\xb9\xd1\x82\xd1\x8b'.decode('utf-8')     # преобразование в строку
 print(x3)
-
-
-
-
