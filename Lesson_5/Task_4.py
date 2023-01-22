b028cd4b5230846f63e2e2ad29fbd38addee4c6d
KateBahur <ebogurina@gmail.com>
2023-01-22 22:46:27 +0300

    work with for, while

---
+++ Lesson_5/Task_4.py
@@ -0,0 +1,17 @@
+# цикл for
+# n = int(input('Введите число n '))
+# s = 0
+# for i in range(1, n+1):
+#     s += i ** 3
+#
+# print(s)
+
+# цикл while
+n = int(input('Введите число n '))
+s = 0
+i = 1
+while i <= n:
+    s += i ** 3
+    i = i + 1
+
+print(s)
