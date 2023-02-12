24e25b5ead48663f0f8fcbd55359c856b0f48f37
KateBahur <ebogurina@gmail.com>
2023-02-12 21:22:15 +0300

    To count the number of letters in the text. The text is located in txt-file

---
+++ lesson_9/task_2.py
@@ -0,0 +1,15 @@
+# ЗАДАНИЕ 3
+
+letter = input('Введите букву: ')
+
+if letter.islower():
+    letter2 = letter.upper()
+else:
+    letter2 = letter.lower()
+
+count = count2 = 0
+with open('text_task2.TXT') as file:
+    for line in file:
+        count += line.count(letter) + line.count(letter2)
+
+print(count)
