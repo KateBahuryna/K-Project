74a6985889531c53992184139917c037bd367e77
KateBahur <ebogurina@gmail.com>
2023-02-21 22:08:53 +0300

    FIXED. Join to dicts, based on the keys

--- lesson_9/task_1.py
+++ lesson_9/task_1.py
@@ -2,15 +2,20 @@
 first_dict = {'a': 1, 'b': 2, 'c': 3}
 second_dict = {'c': 3, 'd': 4, 'e': 5}
 
-for key2, value2 in second_dict.items():
-    if first_dict.get(key2):
-        first_dict[key2] = [first_dict[key2], value2]
+list1 = list(first_dict.keys())
+list2 = list(second_dict.keys())
+list_joined = set(list1 + list2)
+print(list_joined)
+
+result = {}
+for key in list_joined:
+    if first_dict.get(key):
+        first = first_dict[key]
     else:
-        first_dict[key2] = [None, value2]
-
-for key1, value1 in first_dict.items():
-    if not second_dict.get(key1):
-        first_dict[key1] = [value1, None]
-
-merged_dict = first_dict
-print(merged_dict)
+        first = None
+    if second_dict.get(key):
+        second = second_dict[key]
+    else:
+        second = None
+    result[key] = [first, second]
+print(result)
