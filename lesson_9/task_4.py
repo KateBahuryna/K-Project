94d4eaf06abafcc170a76e5fbd9d1b046433b619
KateBahur <ebogurina@gmail.com>
2023-02-21 21:33:47 +0300

    Console program + caching decorator

--- lesson_9/task_4.py
+++ lesson_9/task_4.py
@@ -3,55 +3,106 @@
 # пользователь смог бы просматривать котировки акций компаний из
 # рейтинга sp500: производить поиск по базе компаний, выводить рейтинг,
 # получать среднюю стоимость котировок.
+# +декоратор кэширования
 
-# import csv
-
-
-# def find_info_by_name(name):    # TODO FUNCTION 1
-#     with open('sp500.csv', 'r') as file:
-#         database = csv.DictReader(file)
-#
-#
-# def get_all_companies_by_sector(sector):   # TODO FUNCTION 2
-#     with open('sp500.csv', 'r') as file:
-#         database = csv.DictReader(file)
+import csv
+from statistics import mean
 
 
-# def calculate_average_price():    # TODO FUNCTION 3
-#     with open('sp500.csv', 'r') as file:
-#         database = csv.DictReader(file)
+def decorator(func):
+    decorator_cache = {}
 
-#
-# def get_top10_companies():      # TODO FUNCTION 4
-#     with open('sp500.csv', 'r') as file:
-#         database = csv.DictReader(file)
-#         top10 = sorted(database, key=lambda x: float(x['Price']),
-#         reverse=True)[:10]
-#     return top10
+    def wrapper(*args, **kwargs):
+        if 'result' not in decorator_cache:
+            decorator_cache['result'] = func(*args, **kwargs)
+            print('result saved')
+        else:
+            print('already in memory')
+        return decorator_cache['result']
+    return wrapper
 
 
-choice = int(input("""
-Choose the action from the menu:
-1 - Find info by name
-2 - Get all companies by sector
-3 - Calculate average price
-4 - Get top 10 companies
-5 - Exit
-Your choice: """))
+@decorator
+def find_info_by_name(company_name):
+    with open('sp500.csv', 'r') as file:
+        database = csv.DictReader(file)
+        result = []
+        for row in database:
+            if company_name.lower() in row.get('Name').lower():
+                result.append({
+                    'Symbol': row.get('Symbol'),
+                    'Name': row.get('Name'),
+                    'Sector': row.get('Sector'),
+                    'Stock price': row.get('Price')
+                    }
+                )
+        return result
 
-# if choice == 1:
-#     company_name = input('Input company name: ')
-#     print(find_info_by_name(company_name))
-#
-# if choice == 2:
-#     sector_name = input('Input sector name: ')
-#     print(get_all_companies_by_sector(sector_name))
 
-# if choice == 3:
-#     print(calculate_average_price())
-#
-# if choice == 4:
-#     print(get_top10_companies())
+@decorator
+def get_all_companies_by_sector(sector_name):
+    with open('sp500.csv', 'r') as file:
+        database = csv.DictReader(file)
+        result = []
+        for row in database:
+            if sector_name.lower() in row.get('Sector').lower():
+                result.append(row.get('Name'))
+        return result
 
-# if choice == 5:
-#     print('GOODBYE!')
+
+@decorator
+def calculate_average_price():
+    with open('sp500.csv', 'r') as file:
+        database = csv.DictReader(file)
+        result = []
+        for row in database:
+            result.append(float(row.get('Price')))
+        result_average = mean(result)
+        return result_average
+
+
+@decorator
+def get_top10_companies():
+    with open('sp500.csv', 'r') as file:
+        database = csv.DictReader(file)
+        result = []
+        for row in database:
+            result.append((row.get('Name'),
+                           float(row.get('Price'))))
+        result_sorted = sorted(result, key=lambda price: price[1],
+                               reverse=True)[:10]
+        return result_sorted
+
+
+while True:
+    choice = int(input("Choose the action from the menu:\n"
+                       "1 - Find info by name\n"
+                       "2 - Get all companies by sector\n"
+                       "3 - Calculate average price\n"
+                       "4 - Get top 10 companies\n"
+                       "5 - Exit\n"
+                       "Your choice: "))
+    if choice == 5:
+        print('GOODBYE!')
+        break
+
+    elif choice == 1:
+        company_name = input('Input company name: ')
+        result_choice1 = find_info_by_name(company_name=company_name)
+        print(result_choice1)
+
+    elif choice == 2:
+        sector_name = input('Input sector name: ')
+        result_choice2 = get_all_companies_by_sector(sector_name=sector_name)
+        print(result_choice2)
+
+    elif choice == 3:
+        result_choice3 = calculate_average_price()
+        print(result_choice3)
+
+    elif choice == 4:
+        result_choice4 = get_top10_companies()
+        print(result_choice4)
+
+    else:
+        print('ERROR')
